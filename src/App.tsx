261f064bf22b61b5626115eaaf9722ffac99d754
Andres Elacion III <andreselacioniii@gmail.com>
2025-01-26 14:19:58 +0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./Pages/Home";
+
+const NotFound = () => (
+  <h1 className="text-center mt-10">404 - Page Not Found</h1>
+);
+
+const App: React.FC = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route element={<Home />}>
+          {/* <Route path="Components" element={<Components />} /> */}
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </Router>
+  );
+};
+
+export default App
---
+++ src/Pages/Home.tsx
@@ -0,0 +1,10 @@
+export default function Home() {
+    return (
+        <div className="flex items-center justify-center h-screen bg-gray-100">
+        <div className="text-center">
+            <h1 className="text-4xl font-bold">Welcome to Vite + React + Tailwind CSS</h1>
+            <p className="mt-5 text-lg">Get started by editing the files in <code>src/</code></p>
+        </div>
+        </div>
+    );
+}
